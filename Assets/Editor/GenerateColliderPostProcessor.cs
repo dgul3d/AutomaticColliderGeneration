579429352c5ea2b12bf3a6e931add071580fcbf0
dguliev-github <64034875+dguliev-github@users.noreply.github.com>
2023-11-16 23:55:06 +0200

    Add rotated parents  capability for ColliderPostProccessor

--- Assets/Editor/GenerateColliderPostProcessor.cs
+++ Assets/Editor/GenerateColliderPostProcessor.cs
@@ -1,3 +1,5 @@
+//https://bronsonzgeb.com/index.php/2021/11/27/better-collider-generation-with-asset-processors/
+
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -67,13 +69,30 @@ public class GenerateColliderPostProcessor : AssetPostprocessor
 
         if (DetectNamingConvention(t, "ubx"))
         {
-            AddCollider<BoxCollider>(t);
-            transformsToDestroy.Add(t);
+            if (!HasRotation(t))
+            {
+                AddCollider<BoxCollider>(t);
+                transformsToDestroy.Add(t);
+            }
+            else
+            {
+                // Keep the object but remove MeshFilter and Renderer
+                AddCollider<BoxCollider>(t);
+            }
         }
+
         else if (DetectNamingConvention(t, "ucp"))
         {
-            AddCollider<CapsuleCollider>(t);
-            transformsToDestroy.Add(t);
+            if (!HasRotation(t))
+            {
+                AddCollider<CapsuleCollider>(t);
+                transformsToDestroy.Add(t);
+            }
+            else
+            {
+                // Keep the object but remove MeshFilter and Renderer
+                AddCollider<CapsuleCollider>(t);
+            }
         }
         else if (DetectNamingConvention(t, "usp"))
         {
@@ -87,6 +106,7 @@ public class GenerateColliderPostProcessor : AssetPostprocessor
             collider.convex = true;
             transformsToDestroy.Add(t);
         }
+        //warning - convex mesh colliders are deprecated in Unreal 4 and 5. This option will work with Unity only.
         else if (DetectNamingConvention(t, "umc"))
         {
             TransformSharedMesh(t.GetComponent<MeshFilter>());
@@ -115,23 +135,45 @@ public class GenerateColliderPostProcessor : AssetPostprocessor
 
     T AddCollider<T>(Transform t) where T : Collider
     {
-        T collider = t.gameObject.AddComponent<T>();
-        T parentCollider = t.parent.gameObject.AddComponent<T>();
 
-        EditorUtility.CopySerialized(collider, parentCollider);
-        
-        SerializedObject parentColliderSo = new SerializedObject(parentCollider);
-        var parentCenterProperty = parentColliderSo.FindProperty("m_Center");
-        if (parentCenterProperty != null)
+
+        if (!HasRotation(t))
         {
-            SerializedObject colliderSo = new SerializedObject(collider);
-            var colliderCenter = colliderSo.FindProperty("m_Center");
-            var worldSpaceColliderCenter = t.TransformPoint(colliderCenter.vector3Value);
+            T collider = t.gameObject.AddComponent<T>();
+            T parentCollider = t.parent.gameObject.AddComponent<T>();
+            EditorUtility.CopySerialized(collider, parentCollider);
 
-            parentCenterProperty.vector3Value = t.parent.InverseTransformPoint(worldSpaceColliderCenter);
-            parentColliderSo.ApplyModifiedPropertiesWithoutUndo();
+            SerializedObject parentColliderSo = new SerializedObject(parentCollider);
+            var parentCenterProperty = parentColliderSo.FindProperty("m_Center");
+            if (parentCenterProperty != null)
+            {
+                SerializedObject colliderSo = new SerializedObject(collider);
+                var colliderCenter = colliderSo.FindProperty("m_Center");
+                var worldSpaceColliderCenter = t.TransformPoint(colliderCenter.vector3Value);
+
+                parentCenterProperty.vector3Value = t.parent.InverseTransformPoint(worldSpaceColliderCenter);
+                parentColliderSo.ApplyModifiedPropertiesWithoutUndo();
+            }
+
+            return parentCollider;
         }
+        else
+        {
+            T collider = t.gameObject.AddComponent<T>();
+            GameObject.DestroyImmediate(t.GetComponent<MeshFilter>());
+            GameObject.DestroyImmediate(t.GetComponent<Renderer>());
+            return collider;
+        }
+    }
 
-        return parentCollider;
+    //TODO: While it's possible to use box colliders with rotation of a multiple of 90 deg without rotated parent, script considers them necessary to have one.
+    //Enabling this option requires rewriting transform copying logic in case of inconsistent collider scales, eg (2m,1m,1m) rotated box will transform into (1m,2m,1m)
+    bool HasRotation(Transform t, float rotationTolerance = 0.01f)
+    {
+        Quaternion identity = Quaternion.identity;
+        Quaternion targetRotation = t.rotation;
+
+        // Check if the rotation is close enough to the identity rotation
+        return Quaternion.Angle(targetRotation, identity) > rotationTolerance;
     }
 }
